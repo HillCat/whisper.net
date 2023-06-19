f371f67b4548beae0b7215c47e4aee4578a95e0f
HillCat <41800196+HillCat@users.noreply.github.com>
2023-06-19 21:43:21 +0800

    *)c++版本whisper成功测试

--- examples/SimpleWhisperExample/Program.cs
+++ examples/SimpleWhisperExample/Program.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using FFMpegCore;
 using Whisper.net;
 using Whisper.net.Ggml;
 
@@ -11,7 +13,7 @@ public class Program
     public static async Task Main(string[] args)
     {
         var ggmlType = GgmlType.Base;
-        var modelFileName = "ggml-base.bin";
+        var modelFileName = "ggml-small.en.bin";
         var wavFileName = "kennedy.wav";
 
         if (!File.Exists(modelFileName))
@@ -19,17 +21,37 @@ public class Program
             await DownloadModel(modelFileName, ggmlType);
         }
 
-        using var whisperFactory = WhisperFactory.FromPath("ggml-base.bin");
+        //using var whisperFactory = WhisperFactory.FromPath("ggml-base.bin");
+        using var whisperFactory = WhisperFactory.FromPath("ggml-small.en.bin");
 
         using var processor = whisperFactory.CreateBuilder()
-            .WithLanguage("auto")
+            .WithLanguage("en")
             .Build();
 
+        FFMpeg.ExtractAudio("F:\\Downloads\\Video\\TestWhisperCplus\\20220822020254000.mp4", "F:\\Downloads\\Video\\TestWhisperCplus\\20220822020254000.mp3");
+        FFMpegArguments
+    .FromFileInput("F:\\Downloads\\Video\\TestWhisperCplus\\20220822020254000.mp3")
+    .OutputToFile("F:\\Downloads\\Video\\TestWhisperCplus\\20220822020254000.wav", true, options =>
+        options.WithAudioSamplingRate(16000))
+    .ProcessSynchronously();
+        wavFileName = "F:\\Downloads\\Video\\TestWhisperCplus\\20220822020254000.wav";
         using var fileStream = File.OpenRead(wavFileName);
-
-        await foreach(var result in processor.ProcessAsync(fileStream))
+        bool firstLine = true;
+        await foreach (var result in processor.ProcessAsync(fileStream))
         {
-            Console.WriteLine($"{result.Start}->{result.End}: {result.Text}");
+            var vttTimeStart = TimeSpanToVttTime(result.Start);
+            var vttTimeEnd = TimeSpanToVttTime(result.End);
+            using var writer = new StreamWriter("F:\\Downloads\\Video\\TestWhisperCplus\\20220822020254000.vtt",true);
+            if(firstLine) {
+                writer.WriteLine("WEBVTT");
+                writer.WriteLine($"");
+            }
+            writer.WriteLine($"{result.Start:mm\\:ss\\.fff} --> {result.End:mm\\:ss\\.fff}");
+            //writer.WriteLine($"{vttTimeStart} --> {vttTimeEnd}");
+            writer.WriteLine($"{result.Text}");
+            writer.WriteLine($"");
+            //Console.WriteLine($"{result.Start}-->{result.End}: {result.Text}");
+            firstLine = false;
         }
     }
 
@@ -39,5 +61,17 @@ public class Program
         using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(ggmlType);
         using var fileWriter = File.OpenWrite(fileName);
         await modelStream.CopyToAsync(fileWriter);
+    }
+
+    static string TimeSpanToVttTime(TimeSpan timeSpan)
+    {
+        int milliseconds = (int)timeSpan.TotalMilliseconds;
+        int hours = timeSpan.Hours;
+        int minutes = timeSpan.Minutes;
+        int seconds = timeSpan.Seconds;
+
+        string vttTime = $"{hours:D2}:{minutes:D2}:{seconds:D2},{milliseconds:D3}";
+
+        return vttTime;
     }
 }
